db0d38624e12e648455bf3fd7cb7e4fc38aff621
JaVaD <46402069+toleration110m@users.noreply.github.com>
2026-07-11 08:44:55 +0330

    add error handling

--- RequiredModule.bat
+++ RequiredModule.bat
@@ -1,4 +1,18 @@
-python -m pip install matplotlib
-python -m pip install reportlab
-python -m pip install openpyxl
-python -m pip install test
+@echo off
+echo Starting packages upgrade...
+
+python.exe -m pip install --upgrade pip
+
+python -m pip install --upgrade matplotlib
+python -m pip install --upgrade reportlab
+python -m pip install --upgrade openpyxl
+python -m pip install --upgrade test
+
+if %errorlevel% equ 0 (
+    echo Successfully installed/upgraded packages!
+) else (
+    echo Failed to install/upgrade packages. Error code: %errorlevel%
+)
+echo.
+echo Press any key to close...
+pause > nul
